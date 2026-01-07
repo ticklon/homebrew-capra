2e0314007c5e73b2d7b2e47abc44d3b912fd7095
tkworks <trickysmiler@gmail.com>
2026-01-08 04:05:00 +0900

    Add Capra v1.0.0

---
+++ Casks/capra.rb
@@ -0,0 +1,14 @@
+cask "capra" do
+  version "1.0.0"
+
+  sha256 "3bef51cbe17519a38963222bb7ad9100272e558821844d90dab1558994eca602"
+
+  url "https://github.com/ticklon/capra/releases/download/v1.0.0/Capra_v1.0.0.tar.gz"
+
+  name "Capra"
+  desc "The Background Grazer for Terminal Users"
+  homepage "https://github.com/ticklon/capra"
+
+  app "Capra.app"
+
+end
